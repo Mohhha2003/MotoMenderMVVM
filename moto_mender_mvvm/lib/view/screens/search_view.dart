ce0c61287f9f3da4dd027bee9db0b68f284dc95b
MoBata <mohamedelkarn2003@gmail.com>
2024-05-06 17:37:42 +0300

    store search bar working

--- moto_mender_mvvm/lib/view/screens/search_view.dart
+++ moto_mender_mvvm/lib/view/screens/search_view.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:moto_mender_mvvm/cubits/store_cubit/store_cubit.dart';
+import 'package:moto_mender_mvvm/models/product.dart';
 import 'package:moto_mender_mvvm/view/adapter/favorite_product_adapter.dart';
+import 'package:moto_mender_mvvm/view/widgets/empty_widgets.dart';
 
 class SearchView extends SearchDelegate {
+  List<Product> filtered = [];
+
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
@@ -27,11 +31,42 @@ class SearchView extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    return Center();
+    return ProductsListView(product: filtered);
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Center();
+    final prodcuts = context.read<StoreCubit>().products;
+    if (query == '') {
+      return ProductsListView(product: prodcuts);
+    } else {
+      filtered.clear();
+
+      for (var product in prodcuts) {
+        if (product.name!.toLowerCase().contains(query.toLowerCase())) {
+          filtered.add(product);
+        }
+      }
+      if (filtered.isEmpty) {
+        return const EmptyWidget(text: 'Search');
+      } else {
+        return ProductsListView(product: filtered);
+      }
+    }
+  }
+}
+
+class ProductsListView extends StatelessWidget {
+  const ProductsListView({super.key, required this.product});
+  final List<Product> product;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: product.length,
+      itemBuilder: (context, index) {
+        return FavoritesAdapterCard(product: product[index]);
+      },
+    );
   }
 }
